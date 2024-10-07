8f2465676c35a03d54a802e4d39c74836686685a
Jose Celano <josecelano@gmail.com>
2024-10-07 13:51:42 +0100

    docs: add code review

--- src/io/writer.rs
+++ src/io/writer.rs
@@ -1,5 +1,19 @@
 use std::io;
 
+/* code-review:
+
+   The function `write_byte` only writes:
+
+   - Bytes used in integers:
+     - Digits: '0','1','2','3','4','5','6','7','8','9'
+     - 'e', '-'
+   - JSON reservers chars: '[', ',', ']', '{', ',', ':', '}' defined as constants.
+
+   It could be refactored to be more restrictive. However, in the future we also
+   want to print Bencoded strings as bytes streams, without trying to convert
+   them into UTF-8 strings.
+*/
+
 pub trait Writer {
     /// It writes one byte to the output.
     ///
